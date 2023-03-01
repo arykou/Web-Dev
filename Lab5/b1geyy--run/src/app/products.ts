087f5d603e1e991fa4dcc97b51f9cf538286f780
Ariya <a_kalmagambetova@kbtu.kz>
2023-03-01 20:52:54 +0600

    +10 products

--- Lab5/b1geyy--run/src/app/products.ts
+++ Lab5/b1geyy--run/src/app/products.ts
@@ -22,13 +22,13 @@ export var products = [
   },
   {
     id: 2,
-    name: 'Apple iPhone 12 mini 128Gb Blue',
-    price: 699,
-    description: 'A great phone with one of the best cameras',
-    url:'https://kaspi.kz/shop/p/apple-iphone-12-mini-128gb-sinii-100657856/?c=151010000#!/item',
-    photo: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h6c/hcf/33281467514910/apple-iphone-12-mini-128gb-sinij-100657856-1-Container.jpg',
+    name: 'Smart watch Samsung Galaxy Watch 4 Classic SM-R890NZKACIS 46 mm black-black',
+    price: 204,
+    description: 'Platform Support: Android; Body material: aluminum; Body color: black; Interfaces: Bluetooth, ,NFC',
+    url:'https://kaspi.kz/shop/p/samsung-galaxy-watch-4-classic-sm-r890nzkacis-46-mm-chernyi-chernyi-102170938/?c=151010000#!/item',
+    photo: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h6c/h85/62047597592606/samsung-galaxy-watch-4-classic-sm-r890-cernyj-102170938-1-Container.jpg',
     rat:'★★★★★',
-    category:'Phone',
+    category:'Watch',
     like:0
   },
   {
@@ -49,7 +49,7 @@ export var products = [
     description: 'The iPhone 14 display has rounded corners that follow a beautiful curved design, and these corners are within a standard rectangle.',
     url:'https://kaspi.kz/shop/p/apple-iphone-14-128gb-sirenevyi-106363117/?c=151010000#!/item',
     photo: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h5e/ha3/63072545734686/apple-iphone-14-128gb-sirenevyj-106363117-1.jpg',
-    rat:'★★★★★',
+    rat:'★★★★',
     category:'Phone',
     like:0
   },
@@ -82,7 +82,7 @@ export var products = [
     description: 'AirPods Pro take the listening experience to a new level of individuality',
     url:'https://kaspi.kz/shop/p/apple-airpods-pro-2nd-generation-belyi-106362968/?c=151010000#!/item',
     photo: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hba/hf8/62281477259294/apple-airpods-pro-2nd-generation-belyj-106362968-1.jpg',
-    rat:'★★★★★',
+    rat:'★★★★',
     category:'Headphones',
     like:0
   },
@@ -93,7 +93,7 @@ export var products = [
     description: 'The new iPad Pro has the M2 chip, 12.9-inch Liquid Retina XDR display, 11-inch Liquid Retina display, Wi-Fi 6E, and support for Apple Pencil hover.',
     url:'https://kaspi.kz/shop/p/apple-airpods-max-zelenyi-101161547/?c=151010000#!/item',
     photo: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hd4/h12/33885799677982/apple-airpods-max-zelenyj-101161547-1-Container.jpg',
-    rat:'★★★★',
+    rat:'★★★',
     category:'Headphones',
     like:0
     
@@ -118,6 +118,116 @@ export var products = [
     photo: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h06/h5a/45595063943198/apple-ipad-mini-2021-wi-fi-cellular-64gb-rozovyj-102301187-1-Container.jpg',
     rat:'★★★★★',
     category:'Phone',
+    like:0
+  },
+  {
+    id: 11,
+    name: 'Notebook ASUS TUF Gaming A15 FA506IHRB-HN084 90NR07G7-M008C0 black',
+    price: 839.84,
+    description: 'Good and powerful computer in its class. Quality and price match.',
+    url:'https://kaspi.kz/shop/p/asus-tuf-gaming-a15-fa506ihrb-hn084-90nr07g7-m008c0-chernyi-106255184/?c=151010000#!/item',
+    photo: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h6b/h33/62100002701342/asus-tuf-gaming-a15-fa506ihrb-hn084-90nr07g7-m008c0-cernyj-106255184-1.jpg',
+    rat:'★★★',
+    category:'Laptop',
+    like:0
+  },
+  {
+    id: 12,
+    name: 'Smart watch Huawei WATCH Fit 2 Active blue',
+    price: 147,
+    description: 'Platform support: Android, ,iOS; Body material: plastic; Body color: silver; Screen technology: AMOLED',
+    url:'https://kaspi.kz/shop/p/huawei-watch-fit-2-active-goluboi-105632685/?c=151010000#!/item',
+    photo: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hc2/hfc/62048116310046/huawei-watch-fit-2-goluboj-goluboj-105632685-1.jpg',
+    rat:'★★★★★',
+    category:'Watch',
+    like:0
+  },
+  {
+    id: 13,
+    name: 'Headphones Samsung Galaxy Buds2 Pro (SM-R510NZAACIS) black',
+    price: 159,
+    description: 'Type: headset; Type: in-ear; Connection: wireless; Type of acoustic design: closed; Attachment type: no attachment',
+    url:'https://kaspi.kz/shop/p/samsung-galaxy-buds2-pro-sm-r510nzaacis-chernyi-106128763/?c=151010000#!/item',
+    photo: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/heb/h62/61857930084382/samsung-sm-r510nzaacis-cernyj-106128763-1.jpg',
+    rat:'★★★★',
+    category:'Headphones',
+    like:0
+  },
+  {
+    id: 14,
+    name: 'Headphones Xiaomi Redmi Buds Essential blue',
+    price: 22,
+    description: 'Type: in-ear; Connection: wireless; Type of acoustic design: closed; Attachment type: no attachment',
+    url:'https://kaspi.kz/shop/p/xiaomi-redmi-buds-essential-goluboi-107983822/?c=151010000#!/item',
+    photo: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h52/h66/66744512184350/xiaomi-redmi-buds-essential-goluboi-107983822-1.jpg',
+    rat:'★★★★',
+    category:'Headphones',
+    like:0
+  },
+  {
+    id: 15,
+    name: 'Smart watch Xiaomi Amazfit GTS 4 white-white',
+    price: 133,
+    description: 'Platform support: Android, ,iOS; Body material: aluminum; Body color: white; Screen technology: AMOLED',
+    url:'https://kaspi.kz/shop/p/xiaomi-amazfit-gts-4-belyi-belyi-106677856/?c=151010000#!/item',
+    photo: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h5e/h7f/62875132755998/xiaomi-amazfit-gts-4-belyj-belyj-106677856-1.jpg',
+    rat:'★★★★★',
+    category:'Watch',
+    like:0
+  },
+  {
+    id: 16,
+    name: 'Notebook HP 15s-fq5045ci 6K3C4EA silver',
+    price: 822,
+    description: 'Screen diagonal: 15.6 inch; Processor: Intel Core i5 1235U; Video Card: Intel Iris Xe',
+    url:'https://kaspi.kz/shop/p/hp-15s-fq5045ci-6k3c4ea-serebristyi-106825696/?c=151010000#!/item',
+    photo: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hc6/h67/63141147574302/hp-15s-fq5045ci-6k3c4ea-serebristyj-106825696-1.jpg',
+    rat:'★★★★★',
+    category:'Laptop',
+    like:0
+  },
+  {
+    id: 17,
+    name: 'Headphones JBL Tune 510BT white',
+    price: 52,
+    description: 'Type: headset;Style: overhead; Connection: wireless; Type of acoustic design: closed',
+    url:'https://kaspi.kz/shop/p/jbl-tune-510bt-belyi-101420096/?c=151010000#!/item',
+    photo: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h3d/h52/33973207105566/jbl-tune-510bt-belyj-101420096-1-Container.jpg',
+    rat:'★★★',
+    category:'Headphones',
+    like:0
+  },
+  {
+    id: 18,
+    name: 'Notebook HP 15s-fq5032ci 725W7EA silver',
+    price: 762,
+    description: 'Processor: Intel Core i5 1235U; Video Card: Intel Iris Xe Graphics; RAM size: 8 GB; Hard disk type: SSD',
+    url:'https://kaspi.kz/shop/p/hp-15s-fq5032ci-725w7ea-serebristyi-107923100/?c=151010000#!/item',
+    photo: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h29/h1f/66575448014878/hp-15s-fq5032ci-725w7ea-serebristyi-107923100-1.jpg',
+    rat:'★★★★★',
+    category:'Laptop',
+    like:0
+  },
+  {
+    id: 19,
+    name: 'Smart watch Samsung Galaxy Watch 4 SM-R860 40 mm pink',
+    price: 184,
+    description: 'Platform Support: Android; Body material: aluminum; Body color: golden; Screen technology: Super AMOLED; RAM: 1 GB',
+    url:'https://kaspi.kz/shop/p/samsung-galaxy-watch-4-sm-r860-40-mm-rozovyi-102120670/?c=151010000#!/item',
+    photo: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hb0/h14/44807403274270/samsung-galaxy-watch-4-sm-r860-40-mm-rozovyi-102120670-1.jpg',
+    rat:'★★★★★',
+    category:'Watch',
+    like:0
+  },
+  {
+    id: 20,
+    name: 'Notebook Acer Nitro 5 AN515-46 NH.QGZER.009 black',
+    price: 972,
+    description: 'Processor: AMD Ryzen 7 6800H; Video card: NVIDIA GeForce RTX 3060; RAM size: 16 GB; Hard disk type: SSD',
+    url:'https://kaspi.kz/shop/p/acer-nitro-5-an515-46-nh-qgzer-009-chernyi-106111851/?c=151010000#!/item',
+    photo: 'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h19/hb1/61796027957278/acer-nitro-5-an515-46-nh-qgzer-009-cernyj-106111851-1.jpg',
+    rat:'★★★★★',
+    category:'Laptop',
     like:0
   },
   
